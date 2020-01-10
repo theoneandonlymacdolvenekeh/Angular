99b47924a4a520d5d8d86ddfd91a3947398c1aea
theoneandonlymacdolvenekeh <student.146393@worcesterschools.net>
2020-01-10 13:39:10 -0500

    yjghj
    ghj

--- testapp/src/app/app.component.spec.ts
+++ testapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ToDo-App'`, () => {
+  it(`should have as title 'testapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ToDo-App');
+    expect(app.title).toEqual('testapp');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ToDo-App!');
+    expect(compiled.querySelector('.content span').textContent).toContain('testapp app is running!');
   });
 });
